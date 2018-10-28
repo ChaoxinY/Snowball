37a8dc29173e7b73fe22d9d9d63cd7074a4dd66e
Fatih <f.bagcaci@gmail.com>
2018-10-29 00:17:24 +0100

    Features added: Player can move. Settings Changed: Changed inputmanager for the use of controller.

---
+++ Snowball/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody myRigidbody;
+    private Vector3 dir;
+
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody>();
+        dir = Vector3.zero;
+	}
+	
+	void Update () {
+        // Get player input
+        dir = new Vector3(Input.GetAxis("JoystickLeftHorizontal"), 0, Input.GetAxis("JoystickLeftVertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        // Move player.
+        myRigidbody.velocity = dir.normalized * speed;
+    }
+}
