7a9a98aa9d519776b8167143b3663f4def54675a
Fatih <f.bagcaci@gmail.com>
2018-11-19 19:59:48 +0100

    Feature added:
    Player faces movement direction.

--- Snowball/Assets/Scripts/PlayerMovement.cs
+++ Snowball/Assets/Scripts/PlayerMovement.cs
@@ -5,24 +5,39 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     [SerializeField]
-    private float speed;
+    private float movementSpeed, rotationSpeed;
 
     private Rigidbody myRigidbody;
-    private Vector3 dir;
+    private Vector3 direction;
 
 	void Start () {
         myRigidbody = GetComponent<Rigidbody>();
-        dir = Vector3.zero;
+        direction = Vector3.zero;
 	}
 	
 	void Update () {
         // Get player input
-        dir = new Vector3(Input.GetAxis("JoystickLeftHorizontal"), 0, Input.GetAxis("JoystickLeftVertical"));
+        direction = new Vector3(Input.GetAxis("JoystickLeftHorizontal"), 0, Input.GetAxis("JoystickLeftVertical"));
     }
 
     private void FixedUpdate()
     {
+        move();
+    }
+
+    private void move()
+    {
+        // If player isn't moving do nothing
+        if (direction == Vector3.zero)
+        {
+            return;
+        }
+
         // Move player.
-        myRigidbody.velocity = dir.normalized * speed;
+        myRigidbody.velocity = direction.normalized * movementSpeed;
+
+        // Player faces movement direction.
+        Quaternion newRotaion = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, newRotaion, Time.deltaTime * rotationSpeed);
     }
 }
